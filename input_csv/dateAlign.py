e3fd659b5b4dce06d3b54724e0f2bc78bcaeea51
yan <yanyam.xue@gmail.com>
2024-07-19 19:35:25 -0700

    allowed date normalization for multiple files at a time

--- input_csv/dateAlign.py
+++ input_csv/dateAlign.py
@@ -14,18 +14,17 @@ def normalize_date(date):
         except ValueError: 
             continue 
 
-def write_new_csv(file1): 
+def write_new_csv(file_input_path, file_output_path, output_dir): 
     normalized_dates=[] 
     rows_content=[]
     try: 
-        with open(file1, 'r') as file: 
+        with open(file_input_path, 'r') as file: 
             rows_content = list(csv.DictReader(file)) #multuple files ca prb use ultiprocessing
             fieldnames=rows_content[0].keys() 
             for row in rows_content: 
                 date_str=row["date_column"]
                 normalized_dates.append(normalize_date(date_str))
                 print(normalized_dates)
-      
     except FileNotFoundError:
         print("Error: file not found")
         return 
@@ -33,27 +32,32 @@ def write_new_csv(file1):
     while userInput not in ("y", "n"):
         print("must enter y/n")
     if userInput=="y": 
-        abs_path="/Users/yammy/Desktop/SideQuests/CSV scripting/output_csv" 
-        output_filename="output.csv"
-        output_filepath=os.path.join(abs_path, output_filename) 
-        write_csv(output_filepath, rows_content, normalized_dates, fieldnames)
+        make_csv(file_output_path, rows_content, normalized_dates, fieldnames)
     else:
         print("no new file created")
 
-def write_csv(output_filepath, rows_content, normalized_dates, fieldnames): 
+def make_csv(file_output_path, rows_content, normalized_dates, fieldnames): 
     k=0
     for row in rows_content:
         row["date_column"]=normalized_dates[k]
         k+=1 
-    with open (output_filepath, 'w', newline='') as file: 
+    with open (file_output_path, 'w', newline='') as file: 
         writer = csv.DictWriter(file, fieldnames=fieldnames)
         writer.writeheader() 
         writer.writerows(rows_content)
 
 if __name__=="__main__":
     
-    file1=input("please write the csv file name you would like to normalize the date for: ").strip()
-    while not os.path.isfile(file1): 
-        file1=(str)(input("1st input not a path. please write the csv file name you would like to normalize the date for: ")).strip() 
+    input_dir = input("Please enter the directory containing CSV files to normalize('input_csv'): ").strip().lower()
+    while not os.path.isdir(input_dir):
+        input_dir = input("Invalid directory. Please enter the directory containing CSV files to normalize: ").strip().lower()
     
-    write_new_csv(file1)
+    output_dir = input("Please enter the directory to save normalized CSV files('output_csv'): ").strip().lower()
+    while not os.path.isdir(output_dir):
+        output_dir = input("Invalid directory. Please enter the directory to save normalized CSV files: ").strip().lower()
+    
+    for filename in os.listdir(input_dir):
+        if filename.endswith(".csv"):
+            file_input_path = os.path.join(input_dir, filename)
+            file_output_path = os.path.join(output_dir, filename)
+            write_new_csv(file_input_path, file_output_path, output_dir)
